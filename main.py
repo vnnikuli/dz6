68c537bdd838d087aea131529019a4c7ed7eb4d3
vnnikuli <114097599+vnnikuli@users.noreply.github.com>
2024-01-30 18:21:21 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 4
+
+result = binary_search(arr, target)
+
+if result != -1:
+    print(f"Элемент {target} найден по индексу {result}")
+else:
+    print(f"Элемент {target} отсутствует в массиве")
